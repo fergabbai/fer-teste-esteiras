512a2e479d5d84548dcb8a317141e8dfb4fe0cc9
Fernanda Gabbai <116609378+fergabbai@users.noreply.github.com>
2024-03-15 13:31:57 -0300

    Inclusao de arquivos

---
+++ main.py
@@ -0,0 +1,11 @@
+from soma import soma, mult
+
+def main():
+
+    resultado_soma = soma(1,2)
+    multiplicacao = mult(3, resultado_soma)
+
+    print(multiplicacao)
+
+if __name__ == "__main__":
+    main()
---
+++ test_soma.py
@@ -0,0 +1,7 @@
+from soma import soma, mult
+
+def test_soma():
+    assert soma(1, 2) == 3
+
+def test_mult():
+    assert mult(1, 2) == 2
